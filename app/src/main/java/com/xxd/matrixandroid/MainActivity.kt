8f698f37c88cd2ac3b7a433803a8e6e68c2e9637
xiexude <txsfmg@163.com>
2020-08-04 18:23:11 +0800

    星辰大海，征途不息

---
+++ app/src/main/java/com/xxd/matrixandroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xxd.matrixandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
