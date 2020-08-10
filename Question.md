3ba10c5cb09365fd95f093008ad48244edc6279a
xiexude <txsfmg@163.com>
2020-08-10 16:31:23 +0800

    新增：依赖提取到公共module中

---
+++ Question.md
@@ -0,0 +1,3 @@
+### 问题记录
+1. 为什么 buildscript 中定义的成员变量 ext.kotlin_version 可以在外部引入的 config.build 中找到，而反之不行
+2. 
---
+++ view/src/main/java/com/xxd/view/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.xxd.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvName.text = "我是view module"
+    }
+}
