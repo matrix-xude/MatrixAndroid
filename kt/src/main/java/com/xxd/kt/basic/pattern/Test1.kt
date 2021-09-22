7c2d8cb3e79e6295385d6287025bb7eb548f3333
xxd <txsfmg@163.com>
2021-09-22 15:22:12 +0800

    正则

---
+++ kt/src/main/java/com/xxd/kt/basic/pattern/Test1.kt
@@ -0,0 +1,27 @@
+package com.xxd.kt.basic.pattern
+
+import java.util.regex.Pattern
+
+/**
+ *    author : xxd
+ *    date   : 2021/9/18
+ *    desc   :
+ */
+
+fun main() {
+    m1()
+}
+
+fun m1(){
+    val pattern = Pattern.compile("ABC")
+    val pattern1 = pattern.pattern()
+    println("$pattern1")
+    val matcher = pattern.matcher("ADFCKABCd")
+    val find = matcher.find()
+    println("匹配是否成功 $matcher $find")
+    val start = matcher.start()
+    val end = matcher.end()
+    val group = matcher.group()
+    println("$start $end $group")
+
+}
